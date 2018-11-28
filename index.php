7f72ef6508eb28790f186aab60dd4db7bb368819
root <rgarciaspaules@iesesteveterradas.cat>
2018-11-28 17:29:22 +0100

    creacion de archivos 1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vista Projectos</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+</head>
+<body>
+	<div class="col 12">hola</div>
+
+</body>
+</html>
