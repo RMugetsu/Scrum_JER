6efbf5ad0faff8194553cad68978daa8a006540a
root <rgarciaspaules@iesesteveterradas.cat>
2018-12-03 18:59:38 +0100

    subida de archivos

--- index.php
+++ index.php
@@ -4,10 +4,27 @@
 	<title>Vista Projectos</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
+	<style type="text/css">
+		div{
+			border: 1px red solid;
+		}
+	</style>
 </head>
 <body>
-	<div class="col 12">hola</div>
-
+	<div class="row">
+		<div class="col m10">
+			Adios
+		</div>
+		<div class="col m2">
+			hola
+		</div>
+	</div>
+	<div class="row"></div>
+	<div class="row">
+		<div class="col md1"/>
+		<div class="col md7">
+			Grid
+		</div>
+	</div>
 </body>
 </html>
