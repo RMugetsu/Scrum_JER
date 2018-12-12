e9d9b2eba568ae084d030a616ccd6b52a10032db
Eric <43475531+EricPerLuq@users.noreply.github.com>
2018-12-12 18:41:29 +0100

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
-function generarError(){
+var controlError = 0;
+function generarError(error){
 	var divError = document.querySelector("div[id=error]");
+	if(controlError==0){
+		divError.style.display= "block";
+		controlError++;
+	}
 	var mensaje = document.createElement("LABEL");
 	var contenido = document.createTextNode("Esto es un mensaje de prueba");
-	mensaje.setAttribute("for","Esto es un mensaje de prueba");
+	mensaje.setAttribute("for",'Esto es un mensaje de prueba');
 	var img = document.createElement("IMG");
 	img.setAttribute("src","img/error.png");
 	img.setAttribute("class","ImgError animacion");
@@ -10,4 +15,14 @@ function generarError(){
 	divError.appendChild(img);
 	divError.appendChild(contenido);
 	divError.appendChild(salto);
+	//activarEfecto();
 }
+/*function activarEfecto(){
+	var errores = document.getElementById("error").childNodes;
+	if (errores.length>1) {
+		for (var i = 0; i<errores.length; i++) {
+			errores[i].removeAttribute("class");
+			errores[i].setAttribute("class","ImgError animacion");
+		}	
+	}
+}*/
