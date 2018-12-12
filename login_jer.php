49085cb8bd739c0802601082726400b89acf5013
root <jmartinezmateos@iesesteveterradas.cat>
2018-12-12 19:14:38 +0100

    label y pagina de contraseña olvidada

--- login_jer.php
+++ login_jer.php
@@ -18,7 +18,7 @@
 	session_start();
 
 	if($_POST != null){
-		$connection = mysqli_connect('localhost', 'root','');
+		$connection = mysqli_connect('localhost', 'admin','1234');
 		mysqli_select_db($connection, 'projecte_scrumb');
 
 		$nombre = mysqli_real_escape_string($connection, $_POST['nombre']);
@@ -83,6 +83,9 @@
 		echo "<div class='form-field'>";
 		echo "<label for='password'>Contraseña</label>";
 		echo "<input type='password' name='password'>";
+		echo "<label style='float: right;'>
+				<a class='pink-text' href='mail_recuperacion.php'><b>Olvidaste la contraseña?</b></a>
+				</label><br><br>";
 		echo "</div><br>";
 		echo "<div class='form-field'>";
 		echo "<button class='btn btn-large' style='width:100%;'>LOG IN</button>";
