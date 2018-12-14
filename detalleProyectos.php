d8b8bd24cf1202abb2d5170199a40810fbccaa76
root <jmartinezmateos@iesesteveterradas.cat>
2018-12-14 18:13:15 +0100

    cambio en pantalla de proyecto y eliminada la base de datos

--- detalleProyectos.php
+++ detalleProyectos.php
@@ -5,6 +5,10 @@
 </head>
 <body>
 	<?php
+
+	//ejemplo de insert
+	//INSERT INTO `especificaciones`(`Nombre`,`Horas`, `Dificultad`, `IdUsuario`, `IdProyecto`) VALUES ('Proyecto1',20,'Dificil',1,2);
+
 		echo "$_GET['Nombre']";
 	?>
 </body>
